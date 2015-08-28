c9e7bef7585006feb43d43bd909fb51567e2875a
Kedar Anwardekar <kedar.anwardekar3@gmail.com>
2015-08-28 22:45:41 +0530

    Added some files

---
+++ codes/hr_bfsshortreach.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct link
+{
+	struct link *
+}
+struct link* create_linkingi_table:( int total)
+{
+	
+} 
+
+void main()
+{
+
+}
---
+++ ruby/te.rb
@@ -0,0 +1,7 @@
+def say
+	line = "hello"
+	if line =~ /hello/
+		puts "match"
+	end
+end
+say
